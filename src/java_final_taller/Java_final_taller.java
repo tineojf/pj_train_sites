fcb725be2b678b8a13c4542d98f419f88747512a
tineojf <92743762+tineodev@users.noreply.github.com>
2023-08-24 15:06:48 -0500

    Add fake data

--- src/java_final_taller/Java_final_taller.java
+++ src/java_final_taller/Java_final_taller.java
@@ -21,7 +21,7 @@ public class Java_final_taller {
         // Dimensiones de vagones
         String[][] vagonTurista = {
             {"T01", "T02", "T03"},
-            {"T05", "T06", "T07"},
+            {"T04", "T05", "T06"},
             {"T07", "T08", "T09"}
         };
         String[][] vagonVIP = {
@@ -34,6 +34,9 @@ public class Java_final_taller {
             {"P06", "P07", "P08", "P09", "P10"},
             {"P11", "P12", "P13", "P14", "P15"}
         };
+
+        // Habilitar compra boletos ficticia
+        compraFicticiaAsientos(vagonTurista, vagonVIP, vagonPueblo);
 
         // Mostrar opciones de la aplicacion
         int opc_aplicacion = mostrarOpciones();
@@ -60,6 +63,11 @@ public class Java_final_taller {
         }
     }
 
+    public static void salir() {
+        System.out.println("Fin del programa.");
+        System.exit(0);
+    }
+
     public static int mostrarOpciones() {
         Scanner scanner = new Scanner(System.in);
         int pm_opcion = 0;
@@ -82,6 +90,7 @@ public class Java_final_taller {
     public static void mostrarVagones(String[][] pm_array) {
         int pm_fila = pm_array.length;
         int pm_columna = pm_array[0].length;
+        System.out.println("Asientos disponibles");
 
         for (int i = 0; i < pm_fila; i++) {
             for (int j = 0; j < pm_columna; j++) {
@@ -113,11 +122,20 @@ public class Java_final_taller {
         switch (pm_opcion) {
             case 1:
                 mostrarVagones(vg1);
+                System.out.println("El asiento costará $100");
+                verificarAsientoLibre(vg1);
+                mostrarVagones(vg1);
                 break;
             case 2:
                 mostrarVagones(vg2);
+                System.out.println("El asiento costará $80");
+                verificarAsientoLibre(vg2);
+                mostrarVagones(vg2);
                 break;
             case 3:
+                mostrarVagones(vg3);
+                System.out.println("El asiento costará $10");
+                verificarAsientoLibre(vg3);
                 mostrarVagones(vg3);
                 break;
             case 4:
@@ -128,22 +146,48 @@ public class Java_final_taller {
         }
     }
 
-    public static void salir() {
-        System.out.println("Fin del programa.");
-        System.exit(0);
-    }
-
-    public static void mostrarPasajeros(String[][] pm_array, String pm_tipo) {
+    public static void verificarAsientoLibre(String[][] pm_array) {
+        Scanner scanner = new Scanner(System.in);
         int pm_fila = pm_array.length;
         int pm_columna = pm_array[0].length;
+        boolean asiento_encontrado = false;
+
+        System.out.println("Ingrese el asiento que desea: ");
+        String pm_opcion = scanner.nextLine();
 
         for (int i = 0; i < pm_fila; i++) {
             for (int j = 0; j < pm_columna; j++) {
-                if (!pm_array[i][j].equals("")) {
-                    System.out.println(pm_array[i][j] + " - " + pm_tipo);
+                if (pm_array[i][j].equals(pm_opcion)) {
+                    System.out.println("Asiento seleccionado con exito.");
+                    System.out.println("Ingrese su nombre y apellido para el registro:");
+                    String nombre = scanner.nextLine();
+                    pm_array[i][j] = nombre;
+                    asiento_encontrado = true;
                 }
             }
         }
+
+        if (!asiento_encontrado) {
+            System.out.println("Asiento no encontrado");
+            salir();
+        }
+    }
+
+    public static void compraFicticiaAsientos(String[][] vg1, String[][] vg2, String[][] vg3) {
+        vg1[1][1] = "Timmy";
+        vg1[0][2] = "Alex";
+        vg1[2][2] = "Felix";
+        vg1[1][0] = "Mishi";
+
+        vg2[2][3] = "Oliver";
+        vg2[0][0] = "Monza";
+        vg2[2][0] = "David";
+        vg2[0][2] = "Susan";
+        vg2[1][1] = "Sam";
+
+        vg3[0][4] = "Alisson";
+        vg3[2][0] = "Berny";
+        vg3[1][1] = "Oliv";
     }
 
 }
